24484a8e0c5d4e75a8de6316cbcec3a8eea85fc5
Ollie Charles <ollie@ocharles.org.uk>
2018-02-21 17:34:28 +0000

    Reorganize normalize to have all reduction rules as comments

--- src/normalize.js
+++ src/normalize.js
@@ -343,42 +343,67 @@ function subst(v, e, a) {
 }
 
 function normalize(expr) {
+  // ───────────
+  // Type ⇥ Type
+
   if (expr.type === 'Type') {
     return expr;
   }
 
+  // ───────────
+  // Kind ⇥ Kind
   if (expr.type === 'Kind') {
     return expr;
   }
 
+  // ─────────
+  // x@n ⇥ x@n
   if (expr.type === 'Var') {
     return expr;
   }
 
+  // ───────────
+  // Bool ⇥ Bool
   if (expr.type === 'Bool') {
     return expr;
   }
 
+  // ───────────
+  // True ⇥ True
   if (expr.type === 'True') {
     return expr;
   }
 
+  // ─────────────
+  // False ⇥ False
   if (expr.type === 'False') {
     return expr;
   }
 
+  // t ⇥ True   l₀ ⇥ l₁
+  // ────────────────────────
+  // if t then l₀ else r ⇥ l₁
   if (expr.type === 'BoolIf' && normalize(expr.predicate).type === 'True') {
     return normalize(expr.true);
   }
 
+  // t ⇥ False   r₀ ⇥ r₁
+  // ────────────────────────
+  // if t then l else r₀ ⇥ r₁
   if (expr.type === 'BoolIf' && normalize(expr.predicate).type === 'False') {
     return normalize(expr.false);
   }
 
+  // l ⇥ True   r ⇥ False   t₀ ⇥ t₁
+  // ──────────────────────────────
+  // if t₀ then l else r ⇥ t₁
   if (expr.type === 'BoolIf' && normalize(expr.true).type === 'True' && normalize(expr.false).type === 'False') {
     return normalize(expr.predicate);
   }
 
+  // t₀ ⇥ t₁   l₀ ⇥ l₁   r₀ ⇥ r₁
+  // ─────────────────────────────────────────────
+  // if t₀ then l₀ else r₀ ⇥ if t₁ then l₁ else r₁
   if (expr.type === 'BoolIf') {
     expr.predicate = normalize(expr.predicate);
     expr.true = normalize(expr.true);
@@ -386,54 +411,196 @@ function normalize(expr) {
     return expr;
   }
 
+  // l ⇥ False   r₀ ⇥ r₁
+  // ───────────────────
+  // l || r₀ ⇥ r₁
   if (expr.type === 'BoolOr' && normalize(expr.a).type === 'False') {
     return normalize(expr.b);
   }
 
+  // r ⇥ False   l₀ ⇥ l₁
+  // ───────────────────
+  // l₀ || r ⇥ l₁
   if (expr.type === 'BoolOr' && normalize(expr.b).type === 'False') {
     return normalize(expr.a);
   }
 
+  // l ⇥ True
+  // ─────────────
+  // l || r ⇥ True
   if (expr.type === 'BoolOr' && normalize(expr.a).type === 'True') {
     return { type: 'True' };
   }
 
+  // r ⇥ True
+  // ─────────────
+  // l || r ⇥ True
   if (expr.type === 'BoolOr' && normalize(expr.b).type === 'True') {
     return { type: 'True' };
   }
 
+  // l₀ ⇥ l₁   r₀ ⇥ r₁
+  // ───────────────────
+  // l₀ || r₀ ⇥ l₁ || r₁
   if (expr.type === 'BoolOr') {
     expr.a = normalize(expr.a);
     expr.b = normalize(expr.b);
     return expr;
   }
 
+  // l ⇥ True   r₀ ⇥ r₁
+  // ──────────────────
+  // l && r₀ ⇥ r₁
   if (expr.type === 'BoolAnd' && normalize(expr.a).type === 'True') {
     return normalize(expr.b);
   }
 
+  // r ⇥ True   l₀ ⇥ l₁
+  // ──────────────────
+  // l₀ && r ⇥ l₁
   if (expr.type === 'BoolAnd' && normalize(expr.b).type === 'True') {
     return normalize(expr.a);
   }
 
+  // l ⇥ False
+  // ──────────────
+  // l && r ⇥ False
   if (expr.type === 'BoolAnd' && normalize(expr.a).type === 'False') {
     return { type: 'False' };
   }
 
+  // r ⇥ False
+  // ──────────────
+  // l && r ⇥ False
   if (expr.type === 'BoolAnd' && normalize(expr.b).type === 'False') {
     return { type: 'False' };
   }
 
+  // l₀ ⇥ l₁   r₀ ⇥ r₁
+  // ───────────────────
+  // l₀ && r₀ ⇥ l₁ && r₁
   if (expr.type === 'BoolAnd') {
     expr.a = normalize(expr.a);
     expr.b = normalize(expr.b);
     return expr;
   }
 
+  // l ⇥ True   r₀ ⇥ r₁
+  // ──────────────────
+  // l == r₀ ⇥ r₁
+  if (expr.type === 'BoolEQ' && normalize(expr.a) === 'True') {
+      return normalize(expr.b);
+  }
+
+  // r ⇥ True   l₀ ⇥ l₁
+  // ──────────────────
+  // l₀ == r ⇥ l₁
+  if (expr.type === 'BoolEQ' && normalize(expr.b) === 'True') {
+      return normalize(expr.a);
+  }
+
+  // l ⇥ False   r ⇥ False
+  // ─────────────────────
+  // l == r ⇥ True
+  if (expr.type === 'BoolEQ' && normalize(expr.a) === 'False' && normalize(expr.a) === 'False') {
+      return {
+          type: 'True'
+      };
+  }
+
+  // l₀ ⇥ l₁   r₀ ⇥ r₁
+  // ───────────────────
+  // l₀ == r₀ ⇥ l₁ == r₁
+  if (expr.type === 'BoolEQ') {
+      expr.a = normalize(expr.a);
+      expr.b = normalize(expr.b);
+      return expr;
+  }
+
+  // l ⇥ False   r₀ ⇥ r₁
+  // ──────────────────
+  // l != r₀ ⇥ r₁
+  if (expr.type === 'BoolNE' && normalize(expr.a) === 'False') {
+      return normalize(expr.b);
+  }
+
+  // r ⇥ False   l₀ ⇥ l₁
+  // ──────────────────
+  // l₀ != r ⇥ l₁
+  if (expr.type === 'BoolNE' && normalize(expr.b) === 'False') {
+      return normalize(expr.a);
+  }
+
+  // l ⇥ True   r ⇥ True
+  // ─────────────────────
+  // l != r ⇥ True
+  if (expr.type === 'BoolNE' && normalize(expr.a) === 'True' && normalize(expr.a) === 'True') {
+      return {
+          type: 'False'
+      };
+  }
+
+  // l₀ ⇥ l₁   r₀ ⇥ r₁
+  // ───────────────────
+  // l₀ != r₀ ⇥ l₁ != r₁
+  if (expr.type === 'BoolNE') {
+      expr.a = normalize(expr.a);
+      expr.b = normalize(expr.b);
+      return expr;
+  }
+
+  // ─────────────────
+  // Natural ⇥ Natural
+  if (expr.type === 'Natural') {
+    return expr;
+  }
+
+  // ───────
+  // +n ⇥ +n
   if (expr.type === 'NaturalLit') {
     return expr;
   }
 
+  // f ⇥ Natural/build   a ⇥ Natural/fold b
+  // ──────────────────────────────────────
+  // f a ⇥ b
+  // TODO
+
+  // f ⇥ Natural/fold   a ⇥ Natural/build b
+  // ──────────────────────────────────────
+  // f a ⇥ b
+  // TODO
+
+  // f ⇥ Natural/build   g Natural (λ(x : Natural) → x + +1) +0 ⇥ b
+  // ──────────────────────────────────────────────────────────────
+  // f g ⇥ b
+  // TODO
+
+  // f ⇥ Natural/fold +0 B g   b ⇥ t₁
+  // ────────────────────────────────
+  // f b ⇥ t₁
+  // TODO
+
+  // f ⇥ Natural/fold (+1 + n) B g
+  // g (Natural/fold n B g b) ⇥ t₁
+  // ─────────────────────────────  ; "+1 + n" means "a `Natural` literal greater
+  // f b ⇥ t₁                       ; than `+0`"
+  // TODO
+
+  // l ⇥ +0   r₀ ⇥ r₁
+  // ────────────────
+  // l + r₀ ⇥ r₁
+  // TODO
+
+  // r ⇥ +0   l₀ ⇥ l₁
+  // ────────────────
+  // l₀ + r ⇥ l₁
+  // TODO
+
+  // l ⇥ +m   r ⇥ +n
+  // ───────────────  ; "+m + +n" means "use machine addition"
+  // l + r ⇥ +m + +n
+  // TODO Lacking premise checks
   if (expr.type === 'NaturalPlus') {
     let a = normalize(expr.a);
     let b = normalize(expr.b);
@@ -444,6 +611,88 @@ function normalize(expr) {
     };
   }
 
+  // l₀ ⇥ l₁   r₀ ⇥ r₁
+  // ─────────────────  ; If no other rule matches
+  // l₀ + r₀ ⇥ l₁ + r₁
+  // TODO
+
+  // l ⇥ +0
+  // ──────────
+  // l * r ⇥ +0
+  // TODO
+
+  // r ⇥ +0
+  // ──────────
+  // l * r ⇥ +0
+  // TODO
+
+  // l ⇥ +1   r₀ ⇥ r₁
+  // ────────────────
+  // l * r₀ ⇥ r₁
+  // TODO
+
+  // r ⇥ +1   l₀ ⇥ l₁
+  // ────────────────
+  // l₀ * r ⇥ l₁
+  // TODO
+  //
+  // l ⇥ +m   r ⇥ +n
+  // ───────────────  ; "+m * +n" means "use machine multiplication"
+  // l + r ⇥ +m * +n
+  // TODO
+
+  // l₀ ⇥ l₁   r₀ ⇥ r₁
+  // ─────────────────  ; If no other rule matches
+  // l₀ * r₀ ⇥ l₁ * r₁
+  // TODO
+
+  // f ⇥ Natural/isZero   a ⇥ +0
+  // ───────────────────────────
+  // f a ⇥ True
+  // TODO
+
+  // f ⇥ Natural/isZero   a ⇥ +1 + n
+  // ───────────────────────────────  ; "+1 + n" means "a `Natural` literal
+  // f a ⇥ False                      ; greater than `+0`"
+  // TODO
+
+  // f ⇥ Natural/even   a ⇥ +0
+  // ─────────────────────────
+  // f a ⇥ True
+  // TODO
+
+  // f ⇥ Natural/even   a ⇥ +1
+  // ─────────────────────────
+  // f a ⇥ False
+  // TODO
+
+  // f ⇥ Natural/even
+  // a ⇥ +1 + n
+  // Natural/odd n ⇥ b
+  // ─────────────────  ; "+1 + n" means "a `Natural` literal greater than `+0`"
+  // f a ⇥ b
+  // TODO
+
+  // f ⇥ Natural/odd   a ⇥ +0
+  // ────────────────────────
+  // f a ⇥ False
+  // TODO
+
+  // f ⇥ Natural/odd   a ⇥ +1
+  // ────────────────────────
+  // f a ⇥ True
+  // TODO
+
+  // f ⇥ Natural/odd
+  // a ⇥ +1 + n
+  // Natural/even n ⇥ b
+  // ──────────────────  ; "+1 + n" means "a `Natural` literal greater than `+0`"
+  // f a ⇥ b
+  // TODO
+
+  // f ⇥ Natural/toInteger   a ⇥ +n
+  // ──────────────────────────────
+  // f a ⇥ n
   if (
     expr.type === 'App'
             && normalize(expr.a).type === 'Natural/toInteger'
@@ -455,6 +704,9 @@ function normalize(expr) {
     };
   }
 
+  // f ⇥ Natural/show   a ⇥ +n
+  // ─────────────────────────
+  // f a ⇥ "+n"
   if (
     expr.type === 'App'
             && normalize(expr.a).type === 'Natural/show'
@@ -466,18 +718,246 @@ function normalize(expr) {
     };
   }
 
+  // ─────────────────────────────
+  // Natural/build ⇥ Natural/build
+  // TODO
+
+  // ───────────────────────────
+  // Natural/fold ⇥ Natural/fold
+  // TODO
+
+  // ───────────────────────────────
+  // Natural/isZero ⇥ Natural/isZero
+  // TODO
+
+  // ───────────────────────────
+  // Natural/even ⇥ Natural/even
+  // TODO
+
+  // ─────────────────────────
+  // Natural/odd ⇥ Natural/odd
+  // TODO
+
+  // ─────────────────────────────────────
+  // Natural/toInteger ⇥ Natural/toInteger
   if (expr.type === 'Natural/toInteger') {
     return expr;
   }
 
+  // ───────────────────────────
+  // Natural/show ⇥ Natural/show
   if (expr.type === 'Natural/show') {
     return expr;
   }
 
+  // ───────────
+  // Text ⇥ Text
   if (expr.type === 'Text') {
     return expr;
   }
 
+  // ─────────
+  // "…" ⇥ "…"
+  // TODO
+
+  if (expr.type === 'TextLit') {
+    return expr;
+  }
+
+  // l ⇥ "…"₀   r ⇥ "…"₁
+  // ─────────────────────  ; "…"₀ ++ "…"₁ means "use machine concatenation"
+  // l ++ r ⇥ "…"₀ ++ "…"₁
+  // TODO
+
+  // l ⇥ ""   r₀ ⇥ r₁
+  // ────────────────
+  // l ++ r₀ ⇥ r₁
+  // TODO
+
+  // r ⇥ ""   l₀ ⇥ l₁
+  // ────────────────
+  // l₀ ++ r ⇥ l₁
+  // TODO
+
+  // l₀ ⇥ l₁   r₀ ⇥ r₁
+  // ───────────────────  ; If no other rule matches
+  // l₀ ++ r₀ ⇥ l₁ ++ r₁
+  // TODO
+
+  // ───────────
+  // List ⇥ List
+  if (expr.type === 'List') {
+    return expr;
+  }
+
+  // T₀ ⇥ T₁
+  // ──────────────────────────
+  // [] : List T₀ ⇥ [] : List T₁
+  // TODO
+
+  // t₀ ⇥ t₁   [ ts₀… ] ⇥ [ ts₁… ]
+  // ─────────────────────────────
+  // [ t₀, ts₀… ] ⇥ [ t₁, ts₁… ]
+  // TODO
+
+  // f ⇥ List/fold A₀   a ⇥ List/build A₁ b
+  // ──────────────────────────────────────
+  // f a ⇥ b
+  // TODO
+
+  // f ⇥ List/build A₀   a ⇥ List/fold A₁ b
+  // ──────────────────────────────────────
+  // f a ⇥ b
+  // TODO
+
+  // f ⇥ List/build A
+  // g (List A) (λ(a : A) → λ(as : List A) → [ a ] # as) ([] : List A) ⇥ b
+  // ───────────────────────────────────────────────────────────────────────
+  // f g ⇥ b
+  // TODO
+
+  // f ⇥ List/fold A₀ ([] : List A₁) B g   b₀ ⇥ b₁
+  // ─────────────────────────────────────────────
+  // f b₀ ⇥ b₁
+  // TODO
+
+  // f ⇥ List/fold A₀ [ a, as… ] B g   g a (List/fold A₀ [ as… ] B g b₀) ⇥ b₁
+  // ────────────────────────────────────────────────────────────────────────
+  // f b₀ ⇥ b₁
+  // TODO
+
+  // ls₀ ⇥ [ ls₁… ]
+  // rs₀ ⇥ [ rs₁… ]
+  // [ ls₁… ] # [ rs₁… ] ⇥ t
+  // ───────────────────────   ;  "[ ls₁… ] # [ rs₁… ]" means "use machine
+  // ls₀ # rs₀ ⇥ t             ;  concatenation"
+  // TODO
+
+  // ls ⇥ [] : List T   rs₀ ⇥ rs₁
+  // ────────────────────────────
+  // ls # rs₀ ⇥ rs₁
+  // TODO
+
+  // rs ⇥ [] : List T   ls₀ ⇥ ls₁
+  // ────────────────────────────
+  // ls₀ # rs ⇥ ls₁
+  // TODO
+
+  // ls₀ ⇥ ls₁   rs₀ ⇥ rs₁
+  // ─────────────────────   ; If no other rule matches
+  // ls₀ # rs₀ ⇥ ls₁ # rs₁
+  // TODO
+
+  // f ⇥ List/length A₀   a ⇥ [] : List A₁
+  // ─────────────────────────────────────
+  // f a ⇥ +0
+  // TODO
+
+
+  // f ⇥ List/length A₀   as₀ ⇥ [ a, as₁… ]   +1 + List/length A₀ [ as₁… ] ⇥ n
+  // ─────────────────────────────────────────────────────────────────────────
+  // f as₀ ⇥ n
+  // TODO
+
+  // f ⇥ List/head A₀   as ⇥ [] : List A₁
+  // ────────────────────────────────────
+  // f as ⇥ [] : Optional A₀
+  // TODO
+
+  // f ⇥ List/head A₀   as ⇥ [ a, … ]
+  // ────────────────────────────────
+  // f as ⇥ [ a ] : Optional A₀
+  // TODO
+
+  // f ⇥ List/last A₀   as ⇥ [] : List A₁
+  // ────────────────────────────────────
+  // f as ⇥ [] : Optional A₀
+  // TODO
+
+  // f ⇥ List/last A₀   as ⇥ [ …, a ]
+  // ────────────────────────────────
+  // f as ⇥ [ a ] : Optional A₀
+  // TODO
+
+  // f ⇥ List/indexed A₀   as ⇥ [] : List A₁
+  // ───────────────────────────────────────────────
+  // f as ⇥ [] : List { index : Natural, value : A₀ }
+  // TODO
+
+  // f ⇥ List/indexed A₀   as ⇥ [ a₀, a₁, …, ]
+  // ────────────────────────────────────────────────────────────────────
+  // f as ⇥ [ { index = +0, value = a₀ }, { index = +1, value = a₁ }, … ]
+  // TODO
+
+  // f ⇥ List/reverse A₀   as ⇥ [] : List A₁
+  // ───────────────────────────────────────
+  // f as ⇥ [] : List A₁
+  // TODO
+
+  // f ⇥ List/reverse A₀   as ⇥ [ a₀, a₁, … ]
+  // ────────────────────────────────────────
+  // f as ⇥ [ …, a₁, a₀ ]
+  // TODO
+
+  // ───────────────────────
+  // List/build ⇥ List/build
+  // TODO
+
+  // ─────────────────────
+  // List/fold ⇥ List/fold
+  // TODO
+
+  // ─────────────────────────
+  // List/length ⇥ List/length
+  // TODO
+
+  // ─────────────────────
+  // List/head ⇥ List/head
+  // TODO
+
+  // ─────────────────────
+  // List/last ⇥ List/last
+  // TODO
+
+  // ───────────────────────────
+  // List/indexed ⇥ List/indexed
+  // TODO
+
+  // ───────────────────────────
+  // List/reverse ⇥ List/reverse
+  // TODO
+
+  // ───────────────────
+  // Optional ⇥ Optional
+  if (expr.type === 'Optional') {
+    return expr;
+  }
+
+  // T₀ ⇥ T₁
+  // ───────────────────────────────────
+  // [] : Optional T₀ ⇥ [] : Optional T₁
+  //
+  // t₀ ⇥ t₁   T₀ ⇥ T₁
+  // ───────────────────────────────────────────
+  // [ t₀ ] : Optional T₀ ⇥ [ t₁ ] : Optional T₁
+  // TODO Normalize the type
+  if (expr.type === 'OptionalLit') {
+    expr.value = expr.value === null ? null : normalize(expr.value);
+    return expr;
+  }
+
+  // f ⇥ Optional/fold A₀   a ⇥ Optional/build A₁ b
+  // ──────────────────────────────────────────────
+  // f a ⇥ b
+
+  // f ⇥ Optional/build A₀   a ⇥ Optional/fold A₁ b
+  // ──────────────────────────────────────────────
+  // f a ⇥ b
+
+  // f ⇥ Optional/build A
+  // g (Optional A) (λ(a : A) → [ a ] : Optional A) ([] : Optional A) ⇥ b
+  // ────────────────────────────────────────────────────────────────────
+  // f g ⇥ b
   if (expr.type === 'App'
         && normalize(expr.a).type === 'App'
         && normalize(expr.a).a.type === 'Optional/build'
@@ -521,6 +1001,9 @@ function normalize(expr) {
     });
   }
 
+  // f ⇥ Optional/fold A₀ ([ a ] : Optional A₁) B₀ g   g a ⇥ b₁
+  // ──────────────────────────────────────────────────────────
+  // f b₀ ⇥ b₁
   // TODO More checks the premises hold
   if (expr.type === 'App'
         && normalize(expr.a).type === 'App'
@@ -538,6 +1021,9 @@ function normalize(expr) {
     return expr;
   }
 
+  // f ⇥ Optional/fold A₀ ([] : Optional A₁) B₀ g   b₀ ⇥ b₁
+  // ─────────────────────────────────────────────────────
+  // f b₀ ⇥ b₁
   // TODO More checks the premises hold
   if (expr.type === 'App'
         && normalize(expr.a).type === 'App'
@@ -550,59 +1036,33 @@ function normalize(expr) {
     return normalize(expr.b);
   }
 
-  if (expr.type === 'OptionalLit') {
-    expr.value = expr.value === null ? null : normalize(expr.value);
-    return expr;
-  }
-
-  if (expr.type === 'Optional/build') {
-    return expr;
-  }
-
+  // ─────────────────────────────
+  // Optional/fold ⇥ Optional/fold
   if (expr.type === 'Optional/fold') {
     return expr;
   }
 
-  if (expr.type === 'RecordLit') {
-    Object.keys(expr.fields).forEach(k => {
-      expr.fields[k] = normalize(expr.fields[k]);
-    });
-
+  // ───────────────────────────────
+  // Optional/build ⇥ Optional/build
+  if (expr.type === 'Optional/build') {
     return expr;
   }
 
-  if (expr.type === 'Record') {
-    Object.keys(expr.fields).forEach(k => {
-      expr.fields[k] = normalize(expr.fields[k]);
-    });
-
-    return expr;
-  }
-
+  // ─────────────────
+  // Integer ⇥ Integer
   if (expr.type === 'Integer') {
     return expr;
   }
 
+  // ─────
+  // n ⇥ n
   if (expr.type === 'IntegerLit') {
     return expr;
   }
 
-  if (expr.type === 'List') {
-    return expr;
-  }
-
-  if (expr.type === 'Natural') {
-    return expr;
-  }
-
-  if (expr.type === 'Optional') {
-    return expr;
-  }
-
-  if (expr.type === 'TextLit') {
-    return expr;
-  }
-
+  // f ⇥ Integer/show   a ⇥ n
+  // ────────────────────────
+  // f a ⇥ "n"
   if (expr.type === 'App'
         && normalize(expr.a).type === 'Integer/show'
   ) {
@@ -612,14 +1072,21 @@ function normalize(expr) {
     };
   }
 
+  // ───────────────────────────
+  // Integer/show ⇥ Integer/show
   if (expr.type === 'Integer/show') {
     return expr;
   }
 
+  // ─────────
+  // n.n ⇥ n.n
   if (expr.type === 'DoubleLit') {
     return expr;
   }
 
+  // f ⇥ Double/show   a ⇥ n.n
+  // ─────────────────────────
+  // f a ⇥ "n.n"
   if (
     expr.type === 'App'
             && normalize(expr.a).type === 'Double/show'
@@ -631,22 +1098,37 @@ function normalize(expr) {
     };
   }
 
+  // ─────────────────────────
+  // Double/show ⇥ Double/show
   if (expr.type === 'Double/show') {
     return expr;
   }
 
-  if (expr.type === 'Lambda') {
-    expr.varType = normalize(expr.varType);
-    expr.body = normalize(expr.body);
-    return expr;
-  }
-
+  // A₀ → A₁   B₀ → B₁
+  // ───────────────────────────────
+  // ∀(x : A₀) → B₀ ⇥ ∀(x : A₁) → B₁
   if (expr.type === 'Forall') {
     expr.a = normalize(expr.a);
     expr.b = normalize(expr.b);
     return expr;
   }
 
+  // A₀ → A₁   b₀ → b₁
+  // ───────────────────────────────
+  // λ(x : A₀) → b₀ ⇥ λ(x : A₁) → b₁
+  if (expr.type === 'Lambda') {
+    expr.varType = normalize(expr.varType);
+    expr.body = normalize(expr.body);
+    return expr;
+  }
+
+  // f ⇥ λ(x : A) → b₀
+  // ↑(1, x, 0, a₀) = a₁
+  // b₀[x ≔ a₁] = b₁
+  // ↑(-1, x, 0, b₁) = b₂
+  // b₂ ⇥ b₃
+  // ──────────────────────
+  // f a₀ ⇥ b₃
   if (
     expr.type === 'App'
             && normalize(expr.a).type === 'Lambda'
@@ -661,6 +1143,9 @@ function normalize(expr) {
     return b3;
   }
 
+  // f₀ ⇥ f₁   a₀ ⇥ a₁
+  // ─────────────────  ; If no other rule matches
+  // f₀ a₀ ⇥ f₁ a₁
   if (expr.type === 'App') {
     expr.a = normalize(expr.a);
     expr.b = normalize(expr.b);
@@ -675,6 +1160,23 @@ function normalize(expr) {
   }
 
   if (expr.type === 'Import') {
+    return expr;
+  }
+
+  // Uncategorized
+  if (expr.type === 'RecordLit') {
+    Object.keys(expr.fields).forEach(k => {
+      expr.fields[k] = normalize(expr.fields[k]);
+    });
+
+    return expr;
+  }
+
+  if (expr.type === 'Record') {
+    Object.keys(expr.fields).forEach(k => {
+      expr.fields[k] = normalize(expr.fields[k]);
+    });
+
     return expr;
   }
 
